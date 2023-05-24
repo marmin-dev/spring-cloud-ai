9fd7ea97f62ddf7a176fc1b44108e4eb96d3b824
marmin-dev <marmin.dev@gmail.com>
2023-05-24 09:58:56 +0900

    initialize

---
+++ src/main/java/com/imgai/kaka/KakaApplication.java
@@ -0,0 +1,13 @@
+package com.imgai.kaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imgai/kaka/KakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imgai.kaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
