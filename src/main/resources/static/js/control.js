4e8a6ba8a83df3b77749d527fa9887ff9feff295
marmin-dev <marmin.dev@gmail.com>
2023-05-25 16:16:46 +0900

    css implemented

--- src/main/resources/static/js/control.js
+++ src/main/resources/static/js/control.js
@@ -1,6 +1,5 @@
 const message = document.getElementById("send-text");
 const btn = document.getElementById("send-btn");
 function readO() {
-  message.value = "ai가 생각중입니다";
   message.readOnly = true;
 }
